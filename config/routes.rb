7cb0d3ebe56d724a7055d979430d6047e7af53bc
AfaqShahidKhan <afaqk3203@gmail.com>
2024-05-10 10:41:44 +0500

    update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 Rails.application.routes.draw do
   devise_for :admins
-  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Define the root path route ("/")
+  # Defines the root path route ("/")
   root "home#index"
-  
-  # Define the admin dashboard route
-  authenticated :admin do
+  authenticated :admin_user do
     root to: "admin#index", as: :admin_root
   end
-  
-  # Add other admin routes if needed
-  namespace :admin do
-    # Add your admin routes here
-  end
+  get "admin" => "admin#index"
 end
